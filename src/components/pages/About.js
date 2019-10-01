ec20502c6f2ad4b05fdc1a1c5530103533613492
Lomash Sharma <lomash.sharma@mountblue.io>
2019-10-01 11:31:38 +0530

    Final Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>THis is TodoList app v1.0.0.It is 
+            part of a First React Project
+        </p>
+    </React.Fragment>
+    )
+}
+
+export default About;
